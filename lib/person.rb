3b36fcbea33f1814131e0c5ac6de86a156101ad2
Doug <doug.kerzner@gmail.com>
2021-02-03 16:46:39 -0500

    Doug's edit

--- lib/person.rb
+++ lib/person.rb
@@ -4,12 +4,13 @@ require 'pry'
 class Person
     
     attr_reader :name
-    attr_accessor :balance, :bank_account, :happiness
+    attr_accessor :balance, :bank_account, :happiness, :hygiene
     
     def initialize(name_parameter)
         @name = name_parameter
         @balance = 25
         @happiness = 8
+        @hygiene = 8
         #binding.pry 
         
     end
@@ -19,11 +20,25 @@ class Person
     end
 
     def happiness
-        if @happiness <= 10
-            @happiness
-        end
+        @happiness.clamp(0, 10)    
     end
+
+    def hygiene
+        @hygiene.clamp(0, 10)
+    end 
+
+    def happy?
+        @happiness > 7
+        end 
+
+    def clean?
+        @hygiene > 7
+    end 
     
+    def get_paid(salary_amount) 
+       @balance = self.bank_account + salary_amount
+        "all about the benjamins"
+    end 
 end
 
 
