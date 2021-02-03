b132c6130e25c30b90802b84d5285ea23b4faa75
jayl252574 <jayl252574@gmail.com>
2021-02-03 15:55:57 -0500

    jays.edit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
 # your code goes here
+require 'pry'
+
+class Person
+    
+    attr_reader :name
+    attr_accessor :balance, :bank_account, :happiness
+    
+    def initialize(name_parameter)
+        @name = name_parameter
+        @balance = 25
+        @happiness = 8
+        #binding.pry 
+        
+    end
+
+    def bank_account
+        balance
+    end
+
+    def happiness
+        if @happiness <= 10
+            @happiness
+        end
+    end
+    
+end
+
+
